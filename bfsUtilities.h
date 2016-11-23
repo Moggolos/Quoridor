1d211e81bece9f87155821da211b279b90560b02
EvanKaraf <vaggeliskaraf@gmail.com>
2016-11-23 21:16:16 +0200

    Delete bfsUtilities.h

--- bfsUtilities.h
+++
@@ -1,4 +0,0 @@
-int findPath(char **board,int **visited,int playerRow,int playerCol,int rows,int columns,int winningRow,int distance,int *distanceMin);
-int insert(int currRow,int currCol,int distance);
-int extract(void);
-void emptyQueue(void);
